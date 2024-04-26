612f47915501cfc7290501bada7edbc1b17733d2
MizaelSiebenSilvano <96353670+mizaelsiebensilvano@users.noreply.github.com>
2024-04-26 14:05:46 -0300

    exercicio projeto iMove

---
+++ marco_aurelio/javascript/rodrigo_banas/string.js
@@ -0,0 +1,12 @@
+var nome = "AgileCode";
+
+nome.charAt(2); //"i"
+nome.charCodeAt(0); // 65
+nome.indexOf('e'); // 4
+nome.replace('Code', '!'); // "Agile!"
+nome.split('e'); //['Agil', 'Cod',"];
+
+var nome = "Pedro";
+nome
+nome.charAt nome.charCodeAt
+nome.charAt(2)
